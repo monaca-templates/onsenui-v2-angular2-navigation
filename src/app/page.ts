d298d7b504f95ebb0cce8060cbc93d9ecfdf50ad
Naoki Matagawa <naoki.m@asial.co.jp>
2016-09-17 18:07:08 +0900

    Fixed selector collision problem on page component.

--- src/app/page.ts
+++ src/app/page.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {OnsNavigator} from 'angular2-onsenui';
 
 @Component({
-  selector: 'ons-page',
+  selector: 'ons-page[page]',
   template: require('./page.html'),
   styles: [require('./page.css')]
 })
